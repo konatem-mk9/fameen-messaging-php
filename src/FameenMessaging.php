ed34f90dc7e824c042626505c6af6989773cc5a6
Moussa KONATE <konatem.mk9@gmail.com>
2026-08-17 10:29:12 +0000

    chore(release): 1.0.4
    Doc : prealable WhatsApp (connexion Meta, fenetre 24 h) et cle API couvrant
    les trois canaux. Aucun changement de contrat.

--- src/FameenMessaging.php
+++ src/FameenMessaging.php
@@ -34,7 +34,7 @@ use Fameen\Messaging\Transport\TransportResponse;
  */
 class FameenMessaging
 {
-    public const VERSION = '1.0.3';
+    public const VERSION = '1.0.4';
     public const DEFAULT_BASE_URL = 'https://fameenbusiness.com/api/v1';
 
     private readonly string $apiKey;
